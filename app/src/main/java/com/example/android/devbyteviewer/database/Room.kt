0939c6d695eccf945e5450ada274a3bced7bed6d
madfree <matzefrei@gmail.com>
2020-08-31 13:47:21 +0200

    feat: Implement WorkManager

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -32,18 +32,19 @@ interface VideoDao {
 }
 
 @Database(entities = [DatabaseVideo::class], version = 1)
-abstract class VideosDatabase: RoomDatabase() {
+abstract class VideosDatabase : RoomDatabase() {
     abstract val videoDao: VideoDao
-    private lateinit var INSTANCE: VideosDatabase
+}
 
-    fun getDatabase(context: Context): VideosDatabase {
-        synchronized(VideosDatabase::class.java) {
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
         if (!::INSTANCE.isInitialized) {
             INSTANCE = Room.databaseBuilder(context.applicationContext,
                     VideosDatabase::class.java,
                     "videos").build()
         }
     }
-        return INSTANCE
-    }
+    return INSTANCE
 }
